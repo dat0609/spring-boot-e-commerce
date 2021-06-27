db39333007da4a1ab4d39000016571cb150a1b7a
dat <you@example.com>
2021-06-27 16:18:23 +0700

    export to csv file

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
@@ -3,6 +3,8 @@ package com.shopme.admin.user;
 import java.io.IOException;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.repository.query.Param;
@@ -129,5 +131,15 @@ public class UserController {
 		redirectAttributes.addFlashAttribute("message", msg);
 		
 		return "redirect:/users";
+	}
+	
+	@GetMapping("/users/export/csv")
+	public void exportCSV(HttpServletResponse response) throws IOException {
+		
+		List<User> listUsers = userService.listAll();
+		
+		UserCsvExporter  exporter = new UserCsvExporter();
+		
+		exporter.export(listUsers, response);
 	}
 }
